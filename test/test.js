dc8d1354da848c571a52b949dc272555962bebb1
ismail salih ismail <ismail.ismailov@gmail.com>
2019-03-08 11:59:29 +0200

    Add index.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var request = require("supertest");
 var app = require("../app.js");
 describe("GET /", function() {
- it("respond with Welcome to our App!", function(done) {
+ it("respond with hello world", function(done) {
  
     //navigate to root and check the the response is "Welcome App"
- request(app).get("/").expect("Welcome to our App!", done);
+ request(app).get("/").expect("hello world!", done);
  });
 });
