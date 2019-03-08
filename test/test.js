53bba25bb1853fbcc8b317208603a8a337001ef7
ismail salih ismail <ismail.ismailov@gmail.com>
2019-03-08 12:11:13 +0200

    Edit text

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ describe("GET /", function() {
  it("respond with hello world", function(done) {
  
     //navigate to root and check the the response is "Welcome App"
- request(app).get("/").expect("hello world!", done);
+ request(app).get("/").expect("hello world", done);
  });
 });
