f48db4c335231156b0ec50eed1bb1db21bc618c2
ismail salih ismail <ismail.ismailov@gmail.com>
2019-03-08 11:03:26 +0200

    Test change

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var request = require("supertest");
-var app = require("../app.js");
+var app = require("app.js");
 describe("GET /", function() {
- it("respond with hello world", function(done) {
+ it("respond with Welcome to our App!", function(done) {
  
     //navigate to root and check the the response is "Welcome App"
  request(app).get("/").expect("Welcome to our App!", done);
