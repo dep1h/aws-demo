6600726214b99c2d017d51db25294eaa8ca2ebe6
ismail salih ismail <ismail.ismailov@gmail.com>
2019-03-13 16:39:01 +0200

    Changes

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var request = require("supertest");
 var app = require("../index.js");
 describe("GET /", function() {
- it("respond with hey yo world", function(done) {
+ it("respond with hey world", function(done) {
  
     //navigate to root and check the the response is "Welcome App"
- request(app).get("/").expect("hey yo world", done);
+ request(app).get("/").expect("hey world", done);
  });
 });
